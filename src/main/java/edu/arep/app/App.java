366a6c482ab0b46cbb0fcde2a994804303bbb0de
Edyesid <edyesid2000@gmail.com>
2020-11-04 00:22:43 -0500

    avance2

--- src/main/java/edu/arep/app/App.java
+++ src/main/java/edu/arep/app/App.java
@@ -1,16 +1,21 @@
 package edu.arep.app;
+import edu.arep.app.model.Fibonacci;
+
+import java.math.BigInteger;
+
 import static spark.Spark.*;
 
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         staticFiles.location("/public");
-        get("numbers" ,(request, response) -> {
-            
-           return "numeros";
+        get("/hello/:name", (request, response) -> {
+            int n = Integer.parseInt(request.params(":name"));
+            Fibonacci fib = new Fibonacci();
+            BigInteger ans = fib.Calculate(n);
+            return ans;
         });
     }
-
     static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
